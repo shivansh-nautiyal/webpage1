8c331377938e6cfc5da89a99a2d5b7e6b99f409a
Shiv162003 <120489897+Shiv162003@users.noreply.github.com>
2022-12-31 17:27:10 +0530

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -7,10 +7,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn import preprocessing, svm
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing
+
 
 def load_lottieurl(url):
     r = requests.get(url)
